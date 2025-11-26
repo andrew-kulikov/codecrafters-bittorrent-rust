7c8c1061ef1df2e150d6fdca5842ea86520b3456
andrew-kulikov <andrew.1.kulikov@gmail.com>
2025-11-26 23:51:54 +0100

    Output piece hashes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ mod bencode_parser;
 mod utils;
 
 use std::env;
+use hex;
 
 // Available if you need it!
 // use serde_bencode
+
+fn calculate_piece_hashes(pieces_info_str: String) -> Vec<String> {
+    let pieces_bytes = utils::raw_string_to_bytes(&pieces_info_str);
+    let mut hashes = Vec::new();
+    for chunk in pieces_bytes.chunks(20) {
+        let hash = hex::encode(chunk);
+        hashes.push(hash);
+    }
+    hashes
+}
 
 fn print_torrent_info(info: &serde_json::Value) {
     let announce = info
@@ -13,21 +24,33 @@ fn print_torrent_info(info: &serde_json::Value) {
         .and_then(|v| v.as_str())
         .unwrap_or("")
         .to_string();
-    let length = info
+
+    let info_section = info
         .get("info")
-        .and_then(|info_dict| info_dict.get("length"))
+        .expect("Missing 'info' dictionary in torrent file");
+
+    let length = info_section
+        .get("length")
         .and_then(|v| v.as_u64())
         .unwrap_or(0);
 
-    let encoded_info = bencode_encoder::encode(
-        info.get("info")
-            .expect("Missing 'info' dictionary in torrent file"),
-    );
-
+    let encoded_info = bencode_encoder::encode(info_section);
     let info_hash = utils::compute_sha1_hash(&encoded_info);
+
+    let pieces_info_str = info_section
+        .get("pieces")
+        .and_then(|v| v.as_str())
+        .unwrap_or("")
+        .to_string();
+    let piece_hashes = calculate_piece_hashes(pieces_info_str);
+
     println!("Tracker URL: {}", announce);
     println!("Length: {}", length);
     println!("Info Hash: {}", info_hash);
+    println!("Piece Hashes:");
+    for hash in piece_hashes {
+        println!("{}", hash);
+    }
 }
 
 fn main() {
