e02f6da1baa3373b8ca0060448cb03a0031056d3
andrew-kulikov <andrew.1.kulikov@gmail.com>
2025-12-10 01:19:51 +0100

    Disable interested event send

--- src/main.rs
+++ src/main.rs
@@ -244,7 +244,7 @@ struct MagnetHandshakeHandler {
 
 impl PeerSessionHandler for MagnetHandshakeHandler {
     fn on_connect(&mut self, conn: &PeerConnection) -> anyhow::Result<SessionControl> {
-        conn.send(PeerCommand::Interested)?;
+        //conn.send(PeerCommand::Interested)?;
         if let Some(peer_id) = &conn.peer_id {
             println!("Peer ID: {}", hex::encode(peer_id));
         }
